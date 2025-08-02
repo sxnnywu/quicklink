38d90e08239dbeb1211b37bb25cd9274b84cd056
Sunny Wu <w.sunny0618@gmail.com>
2025-08-02 17:20:58 -0400

    url validation fix

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
+// middleware to parse JSON
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// import required modules to validate URLs
+const dns = require('dns');
+const urlParser = require('url');
+const { error } = require('console');
+
 // root endpoint
 app.get('/', (req, res) => {
   res.send('Welcome to QuickLink!');
@@ -15,33 +24,46 @@ app.get('/', (req, res) => {
 app.post('/api/shorturl', (req, res) => {
   
     // get url from query
-    const { url } = red.body;
+    const { url } = req.body;
 
     // if no url, return error
     if (!url) return res.status(400).json({ error: 'URL is required' });
 
+    // parse url to get hostname
+    const hostname = urlParser.parse(url).hostname;
+
     // validate url
-    const urlRegex = /^(https?:\/\/)?([\w-]+(\.[\w-]+)+)(:\d+)?(\/.*)?$/;
-    if (!urlRegex.test(url)) {
-        return res.status(400).json({ error: 'Invalid URL' });
-    }
+    dns.lookup(hostname, (err, address) => {
+        if(err) return res.status(400).json({ error: 'Invalid URL' });
 
-    // generate short url
-    const shortUrl = Math.random().toString(36).substring(2, 8);
+        // generate short url
+        const shortUrl = generateShortUrl();
 
-    // map short url to original url
-    app.locals.urls = app.locals.urls || {};
-    app.locals.urls[shortUrl] = url;
+        // map short url to original url
+        app.locals.urls = app.locals.urls || {};
+        app.locals.urls[shortUrl] = url;
 
-    // return json response with both urls
-    res.json({
-        original_url: url,
-        short_url: shortUrl
+        // return json response with both urls
+        res.json({
+            original_url: url,
+            short_url: shortUrl
+        });
     });
 });
 
+// generate short url
+function generateShortUrl() {
+
+    let shortUrl;
+    do{
+        shortUrl = Math.floor(Math.random() * 1000000).toString();
+    } while (app.locals.urls && app.locals.urls[shortUrl]);
+
+    return shortUrl;
+}
+
 // redirect endpoint
-app.get('/api/shorturl/:shortUrl', express.json(), (req, res) => {
+app.get('/api/shorturl/:shortUrl', (req, res) => {
     // get short url from params
     const shortUrl = req.params.shortUrl; 
 
